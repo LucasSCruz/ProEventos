697263f900ccc51f0969b65ace99c422385b5bfd
Lucas <lucasscruz2001@gmail.com>
2022-03-24 09:03:41 -0300

    First commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento[] {
+                new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .net 5",
+                Local = "Belo Horizonte",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString()
+
+                }
+            };
+        }
+        private readonly DataContext context;
+
+        public EventoController(DataContext context)
+        {
+            this.context = context;
+
+        }
+    }
+}
